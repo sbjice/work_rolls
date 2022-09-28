5b84fe518c8ada7eb1984746e3e74717a10b9325
sarkhan <boy-ram@mail.ru>
2022-09-28 13:15:40 +0300

    fix writing rolls data

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const { dirname } = require('path');
+const appDir = dirname(require.main.filename);
+
 const {
     existsSync,
     readFileSync,
@@ -5,11 +8,27 @@ const {
     rename,
 } = require('fs');
 
+/*
+const fastcsv = require('fast-csv');
+const fs = require('fs');
+const ws = fs.createWriteStream("out.csv");
+fastcsv
+  .write(data, { headers: true })
+  .pipe(ws);
+*/
 
 const express = require('express');
 const app = express();
 const PORT = 3000;
 const DB_FILE = process.env.DB_FILE || './db.json';
+
+function createProperTimeName(val) {
+    let newTime = val.split('.')[0];
+    const valsArr = newTime.split(' ');
+    let time = valsArr[1];
+    time = time.split(':').join('-');
+    return valsArr[0] + ' ' + time;
+}
 
 
 app.use(express.json())
@@ -17,25 +36,28 @@ app.use(express.json())
 app
     .route('/')
     .get((req, res) => {
+        console.log(req.body, req.headers);
         res.json({
             uid: 1,
             name: 'sb_jice'
         });
     })
     .post((req, res) => {
-        console.log(req.body);
+        console.log(req.body, req.headers);
         if (!req.body.roll_id) {
             res.status(500).send(JSON.stringify('no roll_id'));
             return;
         }
         // const stage = req.body.stage; // start, finish, ongoing;
-        const { roll_id, stage, time, thickness, position } = req.body;
+        let { roll_id, stage, time, thickness, position } = req.body;
+        roll_id = createProperTimeName(roll_id);
         const objToWrite = { time, position, thickness };
-        const fileName = `${roll_id}_write.json`;
+        const fileName = appDir + '\\reports\\' + `${roll_id}_write.json`;
         if (stage === 'start') {
+            console.log(fileName);
             if (existsSync(fileName)) {
                 res.status(500).send(JSON.stringify('file exists'));
-                return
+                return;
             }
             writeFileSync(fileName, 
                 JSON.stringify([objToWrite]), 
@@ -44,6 +66,10 @@ app
                 });
             res.status(200).send(JSON.stringify('created and written successfully'));
         } else if (stage === 'ongoing') {
+            if (!existsSync(fileName)) {
+                res.status(500).send(JSON.stringify('file does not exist'));
+                return;
+            }
             const content = readFileSync(fileName, { encoding: 'utf8' }, function(err) {
                 if ( err ) console.log('ERROR: ' + err);
             });
@@ -60,19 +86,24 @@ app
             if(existsSync(`${roll_id}_complete.json`)) {
                 res.status(500).send(JSON.stringify('file already complete, cannot write anymore'));
                 return;
+            } else if (!existsSync(fileName)) {
+                res.status(500).send(JSON.stringify('file does not exist'));
+                return;
+            } else {
+                const content = readFileSync(fileName, { encoding: 'utf8' }, function(err) {
+                    if ( err ) console.log('ERROR: ' + err);
+                });
+                const records = JSON.parse(content || '[]');
+    
+                writeFileSync(fileName, 
+                    JSON.stringify([ ...records, objToWrite]), 
+                    { encoding: 'utf8' });
+                rename(fileName, appDir + '\\reports\\' + `${roll_id}_complete.json`, function(err) {
+                    if ( err ) console.log('ERROR: ' + err);
+                });
+                res.status(200).send(JSON.stringify('written again and renamed file successfully'));
             }
-            const content = readFileSync(fileName, { encoding: 'utf8' }, function(err) {
-                if ( err ) console.log('ERROR: ' + err);
-            });
-            const records = JSON.parse(content || '[]');
 
-            writeFileSync(fileName, 
-                JSON.stringify([ ...records, objToWrite]), 
-                { encoding: 'utf8' });
-            rename(fileName, `${roll_id}_complete.json`, function(err) {
-                if ( err ) console.log('ERROR: ' + err);
-            });
-            res.status(200).send(JSON.stringify('written again and renamed file successfully'));
         }
     });
 
